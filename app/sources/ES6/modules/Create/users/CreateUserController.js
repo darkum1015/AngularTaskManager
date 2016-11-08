aa649f1b4f4a874f96ee01ccefe4be1bca2ea9db
darkum1015 <darkum09@gmail.com>
2016-11-08 18:14:19 +0530

    User functionality extended to make post request to create new user.

--- app/sources/ES6/modules/Create/users/CreateUserController.js
+++ app/sources/ES6/modules/Create/users/CreateUserController.js
@@ -1,3 +1,52 @@
+"use strict";
 (function () {
+    var appModule = angular.module('app.module');
+
+    appModule.controller('CreateUserController',CreateUserController);
+    CreateUserController.$inject = ['UtilityService','$http'];
+
+    function CreateUserController(UtilityService,$http){
+        var vm = this;
+        vm.newUser = null;
+        vm.userObj ={};
+        vm.userObj.displayName="";
+        vm.userObj.name="";
+        vm.userObj.password="";
+        vm.userObj.admin=false;
+        vm.createUser = createUser;
+
+        class User{
+            constructor(userObj){
+                this.displayName = userObj.displayName;
+                this.name = userObj.name;
+                this.password = userObj.password;
+                this.admin = userObj.admin;
+            }
+        }
+
+        function createUser(){
+            vm.newUser = new User(vm.userObj)
+
+            $http({
+                url: "http://localhost:8300/api/users",
+                method:"PUT",
+                hearders:{
+                    "content-type":"application/json"
+                },
+                data:{
+                    "displayName":vm.newUser.displayName,
+                    "name":vm.newUser.name,
+                    "password":vm.newUser.password,
+                    "admin":vm.newUser.admin
+
+                }
+
+            }).then(function success(response){
+
+            },function failure(error){
+
+            });
+        }
+    }
 
 })();
